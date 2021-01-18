34fef5a6c6d43a1b57d3ec8f8de571feeaf3510b
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-01-19 01:09:17 +0600

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>React Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">New Blog</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
