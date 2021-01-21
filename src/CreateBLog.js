22e79932129f325928e34ae9089f25ff9dc1b9c8
Md. Minhazul Islam <minhazmiraz49@gmail.com>
2021-01-21 19:32:58 +0600

    Redirect to homepage added

--- src/CreateBLog.js
+++ src/CreateBLog.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 const CreateBlog = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
-
   const [isPending, setIsPending] = useState(false);
+
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,7 @@ const CreateBlog = () => {
     }).then(() => {
       console.log("New blog added!!");
       setIsPending(false);
+      history.push("/");
     });
   };
 
